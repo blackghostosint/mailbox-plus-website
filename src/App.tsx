ea035a63cc44712466cdf359100be4c196a704bc
black_ghost <4312279+blackghostosint@users.noreply.github.com>
2025-10-02 14:25:52 -0400

    Updated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -15,19 +15,28 @@ import { USPSServices } from "./pages/USPSServices";
 import { DHLExpress } from "./pages/DHLExpress";
 import { InternationalShippingPage } from "./pages/InternationalShippingPage";
 import { PackageDropOffs } from "./pages/PackageDropOffs";
+import { PackageReceiving } from "./pages/PackageReceiving";
 import { CustomBoxMaking } from "./pages/CustomBoxMaking";
 import { ProfessionalPacking } from "./pages/ProfessionalPacking";
 import { PackagingSupplies } from "./pages/PackagingSupplies";
+import { PostageStamps } from "./pages/PostageStamps";
 
 // Copy & Print
 import { BusinessCardsPage } from "./pages/BusinessCardsPage";
 import { FlyersBrochures } from "./pages/FlyersBrochures";
 import { PostersPrinting } from "./pages/PostersPrinting";
+import { PostcardPrinting } from "./pages/PostcardPrinting";
+import { PosterBannerPrinting } from "./pages/PosterBannerPrinting";
 import { DocumentPrinting } from "./pages/DocumentPrinting";
+import { DocumentFinishing } from "./pages/DocumentFinishing";
+import { GraphicDesign } from "./pages/GraphicDesign";
+import { PrintDocumentServices } from "./pages/PrintDocumentServices";
 import { Copies } from "./pages/Copies";
 
 // Home & Business
 import { MailboxRentalPage } from "./pages/MailboxRentalPage";
+import { DigitalMailboxRental } from "./pages/DigitalMailboxRental";
+import { EveryDoorDirectMail } from "./pages/EveryDoorDirectMail";
 import { Shredding } from "./pages/Shredding";
 import { DocumentScanning } from "./pages/DocumentScanning";
 import { FaxServices } from "./pages/FaxServices";
@@ -54,19 +63,28 @@ const App: React.FC = () => {
         <Route path="/pack-ship/dhl-express" element={<DHLExpress />} />
         <Route path="/pack-ship/international-shipping" element={<InternationalShippingPage />} />
         <Route path="/pack-ship/package-drop-offs" element={<PackageDropOffs />} />
+        <Route path="/pack-ship/package-receiving" element={<PackageReceiving />} />
         <Route path="/pack-ship/custom-box-making" element={<CustomBoxMaking />} />
         <Route path="/pack-ship/professional-packing" element={<ProfessionalPacking />} />
         <Route path="/pack-ship/packaging-supplies" element={<PackagingSupplies />} />
+        <Route path="/pack-ship/postage-stamps" element={<PostageStamps />} />
 
         {/* Copy & Print */}
         <Route path="/copy-print/business-cards" element={<BusinessCardsPage />} />
         <Route path="/copy-print/flyers-brochures" element={<FlyersBrochures />} />
         <Route path="/copy-print/posters-printing" element={<PostersPrinting />} />
+        <Route path="/copy-print/postcard-printing" element={<PostcardPrinting />} />
+        <Route path="/copy-print/poster-banner-printing" element={<PosterBannerPrinting />} />
         <Route path="/copy-print/document-printing" element={<DocumentPrinting />} />
+        <Route path="/copy-print/document-finishing" element={<DocumentFinishing />} />
+        <Route path="/copy-print/graphic-design" element={<GraphicDesign />} />
+        <Route path="/copy-print/print-document-services" element={<PrintDocumentServices />} />
         <Route path="/copy-print/copies" element={<Copies />} />
 
         {/* Home & Business */}
         <Route path="/home-business/mailbox-rental" element={<MailboxRentalPage />} />
+        <Route path="/home-business/digital-mailbox-rental" element={<DigitalMailboxRental />} />
+        <Route path="/home-business/every-door-direct-mail" element={<EveryDoorDirectMail />} />
         <Route path="/home-business/shredding" element={<Shredding />} />
         <Route path="/home-business/document-scanning" element={<DocumentScanning />} />
         <Route path="/home-business/fax-services" element={<FaxServices />} />
