d929f8c6ba429ef6c772591d56be4ccbde6d8e9c
black_ghost <4312279+blackghostosint@users.noreply.github.com>
2025-10-03 06:54:20 -0400

    Updated Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -413,7 +413,7 @@ export const Home: React.FC = () => {
             >
               <div className="rounded-2xl overflow-hidden shadow-lg border border-gray-200">
                 <iframe
-                  src={`https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2990.345!2d${siteConfig.geo.lng}!3d${siteConfig.geo.lat}!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x0!2zNDHCsDM5JzUzLjkiTiA4McKwMTQnNDcuNCJX!5e0!3m2!1sen!2sus!4v1234567890`}
+                  src={`https://www.google.com/maps/embed/v1/place?key=${import.meta.env.VITE_GOOGLE_MAPS_KEY}&q=Mailbox+Plus+Concord+Township+OH`}
                   width="100%"
                   height="450"
                   style={{ border: 0 }}
